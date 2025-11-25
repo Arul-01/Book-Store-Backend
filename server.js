657a779b9d9d773de304351ff14b7447bb896608
Arul-Guru-Prakash <arulguruprakasht@gmail.com>
2025-11-25 16:45:58 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://book-store-frontend-91k3.vercel.app/"
+    "https://book-store-frontend-91k3.vercel.app"
   ],
   credentials: true,
 }));
